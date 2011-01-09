6ce81d76a044a9216175f93473a3c4ee5e1c338f
Michael Gorsuch <michael.gorsuch@gmail.com>
2011-01-08 21:07:20 -0500

    consolidating down to one file.  not sure if I should split into to separate recipes...

--- graphite/recipes/default.rb
+++ graphite/recipes/default.rb
@@ -11,9 +11,86 @@ package "python-cairo"
 package "python-django"
 package "libapache2-mod-wsgi"
 
-include_recipe "graphite::whisper"
-include_recipe "graphite::carbon"
-include_recipe "graphite::web"
+bash "install_whisper" do
+  user "root"
+  cwd "/tmp"
+  code <<-EOH
+  wget "http://launchpad.net/graphite/trunk/0.9.6/+download/whisper-0.9.6.tar.gz"
+  tar xzf whisper-0.9.6.tar.gz
+  rm whisper-0.9.6.tar.gz
+  cd whisper-0.9.6
+  python setup.py install
+  cd ..
+  rm -rf whisper-0.9.6
+  EOH
+end
+
+bash "install_carbon" do
+  user "root"
+  cwd "/tmp"
+  code <<-EOH
+  wget "http://launchpad.net/graphite/trunk/0.9.6/+download/carbon-0.9.6.tar.gz"
+  tar xzf carbon-0.9.6.tar.gz
+  rm carbon-0.9.6.tar.gz
+  cd carbon-0.9.6
+  python setup.py install
+  cd ..
+  rm -rf carbon-0.9.6
+  EOH
+end
+
+bash "configure_carbon" do
+  user "root"
+  cwd "/opt/graphite/conf"
+  code <<-EOH
+  cp carbon.conf.example carbon.conf
+  cp storage-schemas.conf.example storage-schemas.conf
+  EOH
+end
+
+bash "install_graphite-web" do
+  user "root"
+  cwd "/tmp"
+  code <<-EOH
+  wget "http://launchpad.net/graphite/trunk/0.9.6/+download/graphite-web-0.9.6.tar.gz"
+  tar xzf graphite-web-0.9.6.tar.gz
+  rm graphite-web-0.9.6.tar.gz
+  cd graphite-web-0.9.6
+  python setup.py install
+  cd ..
+  rm -rf graphite-web-0.9.6
+  EOH
+end
+
+bash "syncdb" do
+  user "root"
+  cwd "/opt/graphite/webapp/graphite"
+  code <<-EOH
+  python manage.py syncdb --noinput
+  EOH
+end
+
+bash "install_local_settings" do
+  user "root"
+  cwd "/opt/graphite/webapp/graphite"
+  code <<-EOH
+  cp local_settings.py.example local_settings.py
+  EOH
+end
+
+cookbook_file "/opt/graphite/webapp/graphite/graphite.wsgi" do
+  source "graphite.wsgi"
+  mode 0555
+  owner "root"
+  group "root"
+end
+
+cookbook_file "/etc/apache2/sites-enabled/001-graphite" do
+  source "001-graphite"
+  mode 0555
+  owner "root"
+  group "root"
+end
 
 bash "start_carbon" do
   user "root"
