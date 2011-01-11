c0412d2f107043ed1873813fabd8dcf474bcef2a
Michael Gorsuch <michael.gorsuch@gmail.com>
2011-01-10 22:09:34 -0500

    moving paths back into local_settings.py

--- graphite/files/default/local_settings.py
+++ graphite/files/default/local_settings.py
@@ -1,3 +1,20 @@
+import sys, os
+from os.path import join, dirname, abspath
+
+# Filesystem layout (all directores should end in a /)
+WEB_DIR = dirname( abspath(__file__) ) + '/'
+WEBAPP_DIR = dirname( dirname(WEB_DIR) ) + '/'
+GRAPHITE_ROOT = dirname( dirname(WEBAPP_DIR) ) + '/'
+CONTENT_DIR = WEBAPP_DIR + 'content/'
+STORAGE_DIR = '/data/'
+WHISPER_DIR = STORAGE_DIR + 'whisper/'
+RRD_DIR = STORAGE_DIR + 'rrd/'
+LISTS_DIR = STORAGE_DIR + 'lists/'
+INDEX_FILE = STORAGE_DIR + 'index'
+WHITELIST_FILE = LISTS_DIR + 'whitelist'
+LOG_DIR = STORAGE_DIR + 'log/webapp/'
+
+# db configs
 DATABASE_ENGINE = 'sqlite3'                     # 'postgresql', 'mysql', 'sqlite3' or 'ado_mssql'.
 DATABASE_NAME = '/data/db/graphite.db'     # Or path to database file if using sqlite3.
 DATABASE_USER = ''                              # Not used with sqlite3.
