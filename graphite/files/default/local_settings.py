245292b15ea42306348d191398bb24e62aab002c
Michael Gorsuch <michael.gorsuch@gmail.com>
2011-01-10 22:05:48 -0500

    removed locations for the time being

--- graphite/files/default/local_settings.py
+++ graphite/files/default/local_settings.py
@@ -1,20 +1,7 @@
 # Edit this file to override the default graphite settings, do not edit settings.py!!!
 
-WEB_DIR = dirname( abspath(__file__) ) + '/'
-WEBAPP_DIR = dirname( dirname(WEB_DIR) ) + '/'
-GRAPHITE_ROOT = dirname( dirname(WEBAPP_DIR) ) + '/'
-CONTENT_DIR = WEBAPP_DIR + 'content/'
-STORAGE_DIR = GRAPHITE_ROOT + 'storage/'
-WHISPER_DIR = STORAGE_DIR + 'whisper/'
-RRD_DIR = STORAGE_DIR + 'rrd/'
-LISTS_DIR = STORAGE_DIR + 'lists/'
-INDEX_FILE = STORAGE_DIR + 'index'
-WHITELIST_FILE = LISTS_DIR + 'whitelist'
-LOG_DIR = STORAGE_DIR + 'log/webapp/'
-CLUSTER_SERVERS = []
-
 DATABASE_ENGINE = 'sqlite3'                     # 'postgresql', 'mysql', 'sqlite3' or 'ado_mssql'.
-DATABASE_NAME = STORAGE_DIR + 'db/graphite.db'     # Or path to database file if using sqlite3.
+DATABASE_NAME = '/data/db/graphite.db'     # Or path to database file if using sqlite3.
 DATABASE_USER = ''                              # Not used with sqlite3.
 DATABASE_PASSWORD = ''                          # Not used with sqlite3.
 DATABASE_HOST = ''                              # Set to empty string for localhost. Not used with sqlite3.
