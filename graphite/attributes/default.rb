56fa4c5154d6b5dc36033bf09594e94f1e8c436e
Michael Gorsuch <michael.gorsuch@gmail.com>
2011-01-12 21:51:45 -0500

    vhost is now templated out

--- graphite/attributes/default.rb
+++ graphite/attributes/default.rb
@@ -1,3 +1,9 @@
 default.graphite[:dir]            = "/opt/graphite"
+default.graphite[:document_root]  = "/opt/graphite/webapp"
+default.graphite[:log_dir]        = "/data/log"
 default.graphite[:whisper_dir]    = "/data/whisper"
+
 default.graphite[:carbon_user]    = "carbon"
+default.graphite[:wsgi_path]      = "/opt/graphite/webapp/graphite/graphite.wsgi"
+default.graphite[:wsgi_user]      = "www-data"
+default.graphite[:wsgi_group]     = "www-group"
