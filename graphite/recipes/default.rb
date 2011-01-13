de52a250274227d0d2e107a4ccdf1fe9cd4861a6
Michael Gorsuch <michael.gorsuch@gmail.com>
2011-01-12 22:46:20 -0500

    formatting

--- graphite/recipes/default.rb
+++ graphite/recipes/default.rb
@@ -1,12 +1,3 @@
-#
-# Cookbook Name:: graphite
-# Recipe:: default
-#
-# Copyright 2011, YOUR_COMPANY_NAME
-#
-# All rights reserved - Do Not Redistribute
-#
-
 include_recipe "python::cairo"
 include_recipe "python::django"
 
