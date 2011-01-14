3b0302da3e7bc6c2e819a7ab002e3f55e0362996
Michael Gorsuch <michael.gorsuch@gmail.com>
2011-01-13 21:10:55 -0500

    whisper is now a package?

--- graphite/recipes/default.rb
+++ graphite/recipes/default.rb
@@ -46,6 +46,10 @@ remote_file "#{node[:graphite][:deb_cache]}/python-graphite-web_#{node[:graphite
   source "#{node[:graphite][:deb_source]}/python-graphite-web_#{node[:graphite][:graphite_deb_version]}.deb"
 end
 
+dpkg_package "#{node[:graphite][:deb_cache]}/python-whisper_#{node[:graphite][:graphite_deb_version]}.deb" do
+  action :install
+end
+
 bash "install_whisper" do
   not_if {File.exists?('/usr/local/lib/python2.6/dist-packages/whisper.py')}
   user "root"
