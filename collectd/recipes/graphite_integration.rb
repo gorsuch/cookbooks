ba9909adc4fc8ff2eae8a2e988ec0cfe8e44c3b4
Michael Gorsuch <michael.gorsuch@gmail.com>
2011-01-16 14:52:47 -0500

    chef evidently can't link dirs... back to bash for now

--- collectd/recipes/graphite_integration.rb
+++ collectd/recipes/graphite_integration.rb
@@ -1,5 +1,7 @@
 include_recipe "graphite"
 
-link node[:collectd][:rrd_dir] do
-  to "#{node[:graphite][:rrd_dir]}/collectd"
+bash "link_collectd_rrds" do
+  user "root"
+  code "ln -s #{node[:collectd][:rrd_dir]} #{node[:graphite][:rrd_dir]}/collectd"
+  not_if { File.symlink?("#{node[:graphite][:rrd_dir]}/collectd") }
 end
