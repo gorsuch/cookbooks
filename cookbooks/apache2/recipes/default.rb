a1c9381f321f20324c082740701286e6bc581fd2
Michael Gorsuch <michael.gorsuch@gmail.com>
2011-01-16 18:44:05 -0500

    slight header cleanup

--- cookbooks/apache2/recipes/default.rb
+++ cookbooks/apache2/recipes/default.rb
@@ -1,12 +1,3 @@
-#
-# Cookbook Name:: apache2
-# Recipe:: default
-#
-# Copyright 2011, YOUR_COMPANY_NAME
-#
-# All rights reserved - Do Not Redistribute
-#
-
 package "apache2" do
   name "apache2"
 end
