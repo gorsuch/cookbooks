4cd8ffe63bcc6c06e632847b39380c45a56a510b
Michael Gorsuch <michael.gorsuch@gmail.com>
2011-01-16 15:10:48 -0500

    first attempt at serving up collectd properly

--- collectd/attributes/default.rb
+++ collectd/attributes/default.rb
@@ -1,1 +1,2 @@
+default.collectd[:dir]      = "/etc/collectd"
 default.collectd[:rrd_dir]      = "/var/lib/collectd/rrd"
--- collectd/recipes/graphite_integration.rb
+++ collectd/recipes/graphite_integration.rb
@@ -1,5 +1,20 @@
 include_recipe "graphite"
 
+template "#{node[:collectd][:dir]}/collectd.conf" do
+  source "collectd.conf.erb"
+  mode 0644
+  owner "root"
+  group "root"
+end
+
+template "#{node[:collectd][:dir]}/collection.conf" do
+  source "collection.conf.erb"
+  mode 0644
+  owner "root"
+  group "root"
+end
+
+
 bash "link_collectd_rrds" do
   user "root"
   code "ln -s #{node[:collectd][:rrd_dir]} #{node[:graphite][:rrd_dir]}/collectd"
