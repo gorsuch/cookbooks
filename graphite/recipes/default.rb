b199f396cf6f18d695ab8126a508bc09b2bb4754
Michael Gorsuch <michael.gorsuch@gmail.com>
2011-01-16 14:04:39 -0500

    attempt to move to local_settings.py

--- graphite/recipes/default.rb
+++ graphite/recipes/default.rb
@@ -79,8 +79,8 @@ template "/etc/init/carbon.conf" do
   group "root"
 end
 
-template "#{node[:graphite][:dir]}/webapp/graphite/settings.py" do
-  source "settings.py.erb"
+template "#{node[:graphite][:dir]}/webapp/graphite/local_settings.py" do
+  source "local_settings.py.erb"
   mode 0644
   owner "root"
   group "root"
